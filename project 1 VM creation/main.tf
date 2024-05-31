289fbc5fcbea258baa3b5d8bf2cd75c66ad45462
Raj Tilak <85026404+raj-tilak@users.noreply.github.com>
2024-05-31 06:39:21 +0000

    Update main.tf

--- project 1 VM creation/main.tf
+++ project 1 VM creation/main.tf
@@ -3,29 +3,29 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "firstRG" {
+  name     = "RG001"
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+resource "azurerm_virtual_network" "virtualnetwork001" {
+  name                = "VNET001"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.firstRG.location
+  resource_group_name = azurerm_resource_group.firstRG.name
 }
 
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "subnet001" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.firstRG.name
+  virtual_network_name = azurerm_virtual_network.firstRG.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_interface" "netinterface01" {
+  name                = "nic001"
+  location            = azurerm_resource_group.firstRG.location
+  resource_group_name = azurerm_resource_group.firstRG.name
 
   ip_configuration {
     name                          = "internal"
@@ -34,10 +34,10 @@ resource "azurerm_network_interface" "example" {
   }
 }
 
-resource "azurerm_windows_virtual_machine" "example" {
-  name                = "example-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_windows_virtual_machine" "VM001" {
+  name                = "VM001"
+  resource_group_name = azurerm_resource_group.firstRG.name
+  location            = azurerm_resource_group.firstRG.location
   size                = "Standard_F2"
   admin_username      = "adminuser"
   admin_password      = "P@$$w0rd1234!"
